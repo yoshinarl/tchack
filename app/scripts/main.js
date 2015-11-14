b5df15adf52dffd73224226c59a7d98fd6d75167
Atsushi Yoshinari <yoshinari@MacBookPro.local>
2015-11-14 15:48:38 +0900

    Add confirm modal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,10 @@
 // jshint devel:true
 (function($) {
     $('#detail').load('./detail.html');
+
+    $('#button-pay').on('click touchend', function(e) {
+        e.preventDefault();
+
+        location.href = 'payed.html';
+    });
 })(jQuery);
