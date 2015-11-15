fd2d150bf7fa4c3ac696792c92dd250cd02ee9bd
Masato SERIZAWA <sasa.serizawa@gmail.com>
2015-11-15 11:52:53 +0900

    Style a lot of thiings

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -266,7 +266,7 @@
 
         var imageHeight = container.innerWidth() / 6 * 4;
         // open
-        wrapper.animate({ height: container.innerHeight() +  210 + imageHeight });
+        wrapper.animate({ height: container.innerHeight() + imageHeight });
         $("body, html").animate({ scrollTop: $(this).offset().top });
         hiddens.fadeIn();
 
